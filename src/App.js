d806b690eae6bc2f2b805cc555592541d213bbdd
Visnu-Pandian <vsp00002@mix.wvu.edu>
2023-11-12 21:33:09 -0500

    Lab 08 complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+  const [task, setTask] = useState([]);
+  const inputRef = useRef(null);
+
+  const addTask = (e) => {
+    if (e === '') {
+      alert("You cannot have an empty task!");
+    } else {
+      const newItem = inputRef.current.value;
+      if (newItem !== "") {
+        setTask([...task, newItem]);
+      }
+      inputRef.current.value = "";
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+  };
+  
   return (
     <div className="App">
-      <div id="Header" class='header'>
-        <h1>To Do List</h1>
-        <input type="text" id="todoInput" placeholder="Enter Task" />
-        <span onclick="newTask()" class="addBtn"> Add Task</span>
+      <div id="Header" className='header'>
+        <h1 id="Title">ToDo List</h1>
+        <input type="text" id="todoInput" placeholder="Enter Task" ref={inputRef} />
+        <span onClick={addTask} class="addBtn"> Add Task</span>
       </div>
 
       <ul id="todoTasks">
-        <li>test item #1</li>
-        <li>text item #2</li>
+        {task.map((task, index) => (
+          <li className="li-item" key={index}>
+            {task}
+            <span className="text-dark" onClick={() => removeTask(index)}>X</span>
+          </li>))}
       </ul>
     </div>
   );
 }
 
 export default App;
+
+
